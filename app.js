46a9ed9c82cfe498c65576b899b9709e19f7969b
“Giulia <soccolinigiulia@gmail.com>
2021-10-24 16:29:57 +0200

    express+pug backend routes implementation example

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    meta: {
-      data: {
-        title: 'Floema',
-        decription: 'Metadata description'
-      }
-    }
-  })
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail/:id')
 })
 
 app.listen(port, () => {
